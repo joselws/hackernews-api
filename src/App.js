131ff19d3c24b6f65d89fe9f0904f614800760bc
joselws <josel.wilhelms@gmail.com>
2022-01-29 12:23:10 -0400

    bugs fixed and infinite scrolling implemented

--- src/App.js
+++ src/App.js
@@ -13,26 +13,47 @@ function App() {
   // array to store the posts data
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(0);  // starts at page 1
+  const [nextPageAvailable, setNextPageAvailable] = useState(true)
 
   // handles choice between 'all' and 'my favs' buttons
   const changeFavs = (bool_value) => {
+    setPosts([]);
     setFavs(bool_value);
+    setPage(0);
   }
 
   // handles selection of framework from dropdown list
   const changeFramework = (e) => {
     let selected_framework = e.target.value;
     if (framework !== selected_framework) {
-      setFramework(selected_framework);
-      setPage(0);
       setPosts([]);
+      setPage(0);
+      setFramework(selected_framework);
     }
   }
+
+  // infinite scroll
+  window.addEventListener('scroll', () => {
+    // we scrolled down to the bottom of the page
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight && nextPageAvailable) {
+      setNextPageAvailable(false);
+      setPage(page + 1);
+      setInterval(() => {
+        // wait at least 2 seconds until you are able to load the next page
+      }, 2000);
+    }
+
+    // we are not at the bottom of the page
+    if (window.innerHeight + window.scrollY < document.body.offsetHeight) {
+      setNextPageAvailable(true);
+    }
+  })
+
 
   /***** Fetch section *****/
   useEffect(() => {
     fetchPosts();
-  }, [framework, favs, page])
+  }, [page, framework])
 
   // fetches data regarding the selected framework and all/favs choice
   const fetchPosts = async () => {
@@ -47,8 +68,7 @@ function App() {
   const parsePosts = (dataPosts) => {
     const validPosts = [];
     dataPosts.map(post => {
-      if (post.author === null || post.story_title === null || post.story_url === null || post.created_at === null) {
-      } else {
+      if (post.author !== null || post.story_title !== null || post.story_url !== null || post.created_at !== null) {
         validPosts.push(post);
       }
     })
@@ -61,8 +81,10 @@ function App() {
       <Header />
       <Buttons favs={ favs } changeFavs={ changeFavs } />
       <Dropdown framework={ framework } changeFramework={ changeFramework } />
+      { favs ? <p>Displaying favorites</p> : <p>Displaying all</p> }
       { framework ? <p>{ framework }</p> : <p>No framework selected yet</p> }
       { posts.length > 0 ? <p>{ `Number of posts: ${posts.length}` }</p> : <p>No posts</p> }
+      { page >= 0 ? <p>Page { page }</p> : <p></p>}
       <Posts posts={ posts } />
     </div>
   );
